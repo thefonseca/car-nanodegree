94db372995e0328e26dc76a6d1d1d5a759332df8
Marcio Fonseca <marcio@Marcios-MacBook-Pro.local>
2017-04-29 20:28:49 -0300

    Fixed code format

--- CarND-Extended-Kalman-Filter/src/FusionEKF.cpp
+++ CarND-Extended-Kalman-Filter/src/FusionEKF.cpp
@@ -59,68 +59,66 @@ FusionEKF::~FusionEKF() {}
 void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
 
-  /*****************************************************************************
-   *  Initialization
-   ****************************************************************************/
+    /*****************************************************************************
+     *  Initialization
+     ****************************************************************************/
     if (!is_initialized_) {
-    /**
-      * Initialize the state ekf_.x_ with the first measurement.
-      * Create the covariance matrix.
-      * Remember: you'll need to convert radar from polar to cartesian coordinates.
-    */
     
-      // first measurement
-    //cout << "EKF: " << endl;
-    //ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
-      
-    ekf_.P_ << 1, 0, 0, 0,
-          0, 1, 0, 0,
-          0, 0, 1000, 0,
-          0, 0, 0, 1000;
-
-    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
         /**
-         Convert radar from polar to cartesian coordinates and initialize state.
-         */
+          * Initialize the state ekf_.x_ with the first measurement.
+          * Create the covariance matrix.
+          * Remember: you'll need to convert radar from polar to cartesian coordinates.
+        */
         
-        float ro = measurement_pack.raw_measurements_(0);
-        float phi = measurement_pack.raw_measurements_(1);
-        ekf_.x_(0) = ro * cos(phi);
-        ekf_.x_(1) = ro * sin(phi);
-    
-    } else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
-        /**
-         Initialize state.
-         */
-        ekf_.x_(0) = measurement_pack.raw_measurements_(0);
-        ekf_.x_(1) =measurement_pack.raw_measurements_(1);
+        // first measurement
+        //cout << "EKF: " << endl;
+        ekf_.x_ << 1, 1, 1, 1;
+          
+        ekf_.P_ << 1, 0, 0, 0,
+              0, 1, 0, 0,
+              0, 0, 1000, 0,
+              0, 0, 0, 1000;
+
+        if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+            /**
+             Convert radar from polar to cartesian coordinates and initialize state.
+             */
+            
+            float ro = measurement_pack.raw_measurements_(0);
+            float phi = measurement_pack.raw_measurements_(1);
+            ekf_.x_(0) = ro * cos(phi);
+            ekf_.x_(1) = ro * sin(phi);
+        
+        } else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+            /**
+             Initialize state.
+             */
+            ekf_.x_(0) = measurement_pack.raw_measurements_(0);
+            ekf_.x_(1) =measurement_pack.raw_measurements_(1);
+        }
+          
+        previous_timestamp_ = measurement_pack.timestamp_;
+
+        // done initializing, no need to predict or update
+        is_initialized_ = true;
+        return;
     }
-      
-    previous_timestamp_ = measurement_pack.timestamp_;
 
-    // done initializing, no need to predict or update
-    is_initialized_ = true;
-    return;
-  }
+    /*****************************************************************************
+     *  Prediction
+     ****************************************************************************/
 
-  /*****************************************************************************
-   *  Prediction
-   ****************************************************************************/
-
-  /**
-   TODO:
+    /**
      * Update the state transition matrix F according to the new elapsed time.
       - Time is measured in seconds.
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
-   */
+     */
     
     //compute the time elapsed between the current and previous measurements
     float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
     previous_timestamp_ = measurement_pack.timestamp_;
     
-    // TODO: YOUR CODE HERE
     //1. Modify the F matrix so that the time is integrated
     ekf_.F_ << 1, 0, dt, 0,
                0, 1, 0, dt,
