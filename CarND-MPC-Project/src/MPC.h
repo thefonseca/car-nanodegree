b3b7a12fcbf5b88baf1872aac5aca0fbbbd178a8
Marcio Fonseca <thefonseca@gmail.com>
2017-06-30 15:29:30 -0300

    MPC complete

--- CarND-MPC-Project/src/MPC.h
+++ CarND-MPC-Project/src/MPC.h
@@ -7,14 +7,17 @@
 using namespace std;
 
 class MPC {
- public:
-  MPC();
-
-  virtual ~MPC();
-
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+public:
+    MPC();
+    
+    virtual ~MPC();
+    
+    // Solve the model given an initial state and polynomial coefficients.
+    // Return the first actuatotions.
+    vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+    
+    vector<double> predx;
+    vector<double> predy;
 };
 
 #endif /* MPC_H */
